f208ec2fbed11fdecc7f3e1c5f29a5fef21c320c
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2026-04-08 22:09:14 +0530

    [Divanshu] Add. create middleware to verify

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+exports.authMiddleware = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({ error: "No token provided" });
+    }
+
+    const decoded = jwt.verify(token, "secret123");
+
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
